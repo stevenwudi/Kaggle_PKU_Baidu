6d861ffde4ee931c7e4cd3cf522f137c083642fa
stevenwudi <stevenwudi@gmail.com>
2019-11-07 18:30:20 +0800

    Merge.

--- configs/htc/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e.py
+++ configs/htc/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e.py
@@ -101,17 +101,7 @@ model = dict(
         roi_layer=dict(type='RoIAlign', out_size=14, sample_num=2),
         out_channels=256,
         featmap_strides=[8]),
-    semantic_head=dict(
-        type='FusedSemanticHead',
-        num_ins=5,
-        fusion_level=1,
-        num_convs=4,
-        in_channels=256,
-        conv_out_channels=256,
-        num_classes=183,
-        ignore_label=255,
-        loss_weight=0.2))
-# model training and testing settings
+steveing settings
 train_cfg = dict(
     rpn=dict(
         assigner=dict(
