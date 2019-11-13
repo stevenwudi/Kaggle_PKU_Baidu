afdf7a7d383279957b365304bb56974126060a46
stevenwudi <stevenwudi@gmail.com>
2019-11-13 11:21:17 +0800

    dist_train code added as a single bash line.

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,3 +7,8 @@ GPUS=$2
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+
+
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 python -m torch.distributed.launch --nproc_per_node=6 train_kaggle_pku.py --launcher pytorch
+
+
