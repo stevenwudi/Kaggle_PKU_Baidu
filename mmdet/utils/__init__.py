ba8790bdf43eed5f60a73c11ef4315789fb6cbfc
yihao <o0o@o0oo0o.cc>
2019-12-12 11:32:55 +0800

    move map_calculatioin to mmdet.utils

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,4 +1,5 @@
 from .flops_counter import get_model_complexity_info
 from .registry import Registry, build_from_cfg
+from .map_calculation import check_match, RotationDistance, TranslationDistance, str2coords, expand_df, coords2str
 
 __all__ = ['Registry', 'build_from_cfg', 'get_model_complexity_info']
