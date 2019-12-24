f4a37751ebec2877c2bfe691c42c7b9ed9236790
yyjabiding <791223583@qq.com>
2019-12-24 12:01:34 +0800

    minor fix

--- mmdet/utils/map_calculation.py
+++ mmdet/utils/map_calculation.py
@@ -75,7 +75,8 @@ def RotationDistance(p, g):
 
 thres_tr_list = [0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01]
 thres_ro_list = [50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
-
+delta_x = 1692 - 1686.2379
+fx = 2304.5479
 
 def check_match(idx, train_df, valid_df,flip_mode=False):
     keep_gt = False
